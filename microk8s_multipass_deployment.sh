b46db35539baef86aa8ab6f33ae6f5adea0950e2
Bryan Thode <bryan@tatq.com>
2023-05-20 06:50:48 -0500

    Fix variable ordering

--- microk8s_multipass_deployment.sh
+++ microk8s_multipass_deployment.sh
@@ -6,10 +6,10 @@ mem="8G"
 disk="20GB"
 vm_name="microk8s"
 local_launch_configuration="microk8s-config.yaml"
-tmp_path="/tmp/{$launch_configuration_name}"
 launch_configuration_dir="/root/snap/microk8s/common/"
 launch_configuration_name=".microk8s.yaml"
 launch_configuration_path="${launch_configuration_dir}${launch_configuration_name}"
+tmp_path="/tmp/${launch_configuration_name}"
 argocd_install_manifest="https://raw.githubusercontent.com/argoproj/argo-cd/master/manifests/install.yaml"
 
 multipass launch "${image}" --name "${vm_name}" --cpus "${cpus}" --memory "${mem}" --disk "${disk}"
@@ -21,7 +21,6 @@ multipass exec "${vm_name}" -- sudo cp "${tmp_path}" "${launch_configuration_pat
 multipass exec "${vm_name}" -- sudo snap install microk8s --classic --channel 1.27
 multipass exec "${vm_name}" -- sudo microk8s status --wait-ready
 multipass exec "${vm_name}" -- sudo microk8s kubectl wait --namespace metallb-system --for=condition=Available deployment/controller
-
 
 multipass exec "${vm_name}" -- sudo microk8s kubectl apply -f "${argocd_install_manifest}"
 multipass exec "${vm_name}" -- sudo microk8s kubectl wait --for=condition=Available deployment/argocd-server --timeout=5m
